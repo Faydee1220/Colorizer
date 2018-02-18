4f5f43ab08e501a2d36fcfdc08e1cd8b92f921d9
Faydee <faydee1220@gmail.com>
2018-02-18 10:44:39 +0800

    新增選單點擊功能

--- app/src/main/java/com/teamtreehouse/colorizer/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/colorizer/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -36,11 +37,69 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuItem menuItem = menu.add("Next Image"); // 預設會是右上角直的 ...
-        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS); // 顯示成按鈕圖示
-        menuItem.setIcon(R.drawable.ic_add_a_photo_black_24dp);
-        menuItem.getIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP); // 非透明處改為白色
+        // 用 Code 寫 menu
+//        MenuItem menuItem = menu.add("Next Image"); // 預設會是右上角直的 ...
+//        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS); // 顯示成按鈕圖示
+//        menuItem.setIcon(R.drawable.ic_add_a_photo_black_24dp);
+//        menuItem.getIcon().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP); // 非透明處改為白色
+
+        // 用 xml 介面設計 menu
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        // 將相機按鈕改為白色
+        Drawable nextImageDrawable = menu.findItem(R.id.nextImage).getIcon();
+        nextImageDrawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+
+        // 將 check box 設為預設已經打勾
+        menu.findItem(R.id.red).setChecked(red);
+        menu.findItem(R.id.green).setChecked(green);
+        menu.findItem(R.id.blue).setChecked(blue);
+
+        // 當點選 color 按鈕時，因為有 invalidateOptionsMenu()，會來此檢查是否顯示顏色的選項
+        menu.setGroupVisible(R.id.colorGroup, color);
+
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nextImage:
+                imageIndex += 1;
+                if (imageIndex >= imageResIds.length) {
+                    imageIndex = 0;
+                }
+                loadImage();
+                break;
+            case R.id.color:
+                color = !color;
+                updateSaturation();
+                invalidateOptionsMenu(); // 會重新呼叫 onCreateOptionsMenu
+                break;
+            case R.id.red:
+                red = !red;
+                updateColors();
+                item.setChecked(red);
+                break;
+            case R.id.green:
+                green = !green;
+                updateColors();
+                item.setChecked(green);
+                break;
+            case R.id.blue:
+                blue = !blue;
+                updateColors();
+                item.setChecked(blue);
+                break;
+            case R.id.reset:
+                imageView.clearColorFilter();
+                red = green = blue = color = true;
+                invalidateOptionsMenu();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void updateSaturation() {
